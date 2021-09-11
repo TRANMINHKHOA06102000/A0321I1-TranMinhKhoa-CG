b9eafe9edd9f111e1526e253649f13e899e746ca
TRANMINHKHOA06102000 <khoa4755@gmail.com>
2021-09-11 13:05:49 +0700

    up casestudy sql yeu cau 1

---
+++ module3/casestudy_sql/create_database.sql
@@ -0,0 +1,109 @@
+create database case_study_sql;
+use case_study_sql;
+-- drop database case_study_sql;
+create table vitri(
+	idViTri int auto_increment not null,
+    tenViTri varchar(45) not null,
+    primary key(idViTri)
+);
+create table trinhdo(
+	idTrinhDo int auto_increment not null,
+    trinhDo varchar(45) not null,
+    primary key(idTrinhDo)
+);
+create table bophan(
+	idBoPhan int auto_increment not null,
+    tenBoPhan varchar(45) not null,
+    primary key(idBoPhan)
+);
+create table nhanvien(
+	idNhanVien int auto_increment,
+    hoTen varchar(50) not null,
+    idViTri int not null,
+    idTrinhDo int not null,
+    idBoPhan int not null,
+    ngaySinh date,
+    soCMTND varchar(15),
+    luong double,
+    sdt char(10),
+    email varchar(45),
+    diaChi varchar(45),
+    primary key(idNhanVien),
+    foreign key(idViTri) references vitri(idViTri),
+    foreign key(idTrinhDo) references trinhdo(idTrinhDo),
+    foreign key(idBoPhan) references bophan(idBoPhan)
+);
+create table loaikhach(
+	idLoaiKhach int auto_increment not null,
+    tenLoaiKhach varchar(45) not null,
+    primary key(idLoaiKhach)
+);
+create table khachhang(
+	idKhachHang int auto_increment not null,
+    idLoaiKhach int not null,
+    hoTen varchar(45) not null,
+    ngaySinh date,
+    soCMTND varchar(15),
+    sdt char(10),
+    email varchar(45),
+    diaChi varchar(45),
+    primary key(idKhachHang),
+    foreign key(idLoaiKhach) references loaikhach(idLoaiKhach)
+);
+create table kieuthue(
+	idKieuThue int auto_increment not null,
+    tenKieuThue varchar(45) not null,
+    gia int not null,
+    primary key(idKieuThue)
+);
+create table loaidichvu(
+	idLoaiDichVu int auto_increment not null,
+    tenLoaiDichVu varchar(45) not null,
+    primary key(idLoaiDichVu)
+);
+create table dichvu(
+	idDichVu int auto_increment not null,
+    tenDichVu varchar(45) not null,
+    dienTich int,
+    sotang int,
+    soNguoiToiDa int,
+    chiPhiThue double,
+    idKieuThue int not null,
+    idLoaiDichVu int not null,
+    trangThai varchar(45),
+    primary key(idDichVu),
+    foreign key(idKieuThue) references kieuthue(idKieuThue),
+    foreign key(idLoaiDichVu) references loaidichvu(idLoaiDichVu)
+);
+create table dichvudikem(
+	idDichVuDiKem int auto_increment not null,
+    tenDichVuDiKem varchar(45) not null,
+    gia int not null,
+    donVi varchar(15) not null,
+    trangThaiKhaDung varchar(45),
+    primary key(idDichVuDiKem)
+);
+create table hopdong(
+	idHopDong int auto_increment not null,
+    idNhanvien int not null,
+    idKhachHang int not null,
+    idDichVu int not null,
+    ngayLamHopDong date not null,
+    ngayKetThuc date not null,
+    tienDatCoc double,
+    tongTien double,
+    primary key(idHopDong),
+    foreign key(idNhanvien) references nhanvien(idNhanvien),
+    foreign key(idKhachHang) references khachhang(idKhachHang),
+    foreign key(idDichVu) references dichvu(idDichVu)
+);
+create table hopdongchitiet(
+	idHopDongChiTiet int auto_increment not null,
+    idHopDong int not null,
+    idDichVuDiKem int not null,
+    soLuong int not null,
+    primary key(idHopDongChiTiet),
+    foreign key(idHopDong) references hopdong(idHopDong),
+    foreign key(idDichVuDiKem) references dichvudikem(idDichVuDiKem)
+);
+
---
+++ module3/casestudy_sql/insert_database.sql
@@ -0,0 +1,60 @@
+insert into vitri(tenViTri) values
+('le tan'),
+('phuc vu'),
+('chuyen vien'),
+('giam sat'),
+('quan ly'),
+('giam doc');
+insert into trinhdo(trinhDo) values
+('trung cap'),
+('cao dang'),
+('dai hoc'),
+('sau dai hoc');
+insert into bophan(tenBoPhan) values
+('sale – marketing'),
+('hanh chinh'),
+('phuc vu'),
+('quan ly');
+insert into nhanvien(hoTen,idViTri,idTrinhDo,idBoPhan,ngaySinh,soCMTND,luong,sdt,email,diaChi) values
+('Hoa Vo Khuyet',4,3,1,'2000-12-06','123123123456',10000000,'0357740063','khoa45@gmail.com','Da Nang'),
+('Tran Minh Khoa',5,3,2,'2000-10-06','123123123459',10000000,'0357740062','khoa415@gmail.com','Quang Ngai'),
+('Le Anh Minh',6,3,4,'2008-12-06','123123123458',8000000,'0357740061','khoa452@gmail.com','Quang Tri');
+insert into loaikhach(tenLoaiKhach) values
+('Diamond'),
+('Platinium'),
+('Gold'),
+('Silver'),
+('Member');
+insert into khachhang(idLoaiKhach,hoTen,ngaySinh,soCMTND,sdt,email,diaChi) values
+(1,'Truong Tan Hai','1998-02-02','123123123477','0957740069','hai@gmail.com','Da Nang'),
+(2,'Truong Tan Beo','1998-02-02','923123123479','0997740069','hai1@gmail.com','Quang Ngai'),
+(1,'Truong Tan Vui','1998-02-02','723123123437','0657740059','hai2@gmail.com','Da Nang');
+insert into kieuthue(tenKieuThue, gia) values
+('Year', 105000), 
+('Month', 35000), 
+('Day', 25000),
+('Hour', 7984);
+insert into loaidichvu(tenLoaiDichVu) values 
+('Villa'), 
+('House'), 
+('Room');
+insert into dichvu(tenDichVu,dienTich,sotang,soNguoiToiDa,chiPhiThue,idKieuThue,idLoaiDichVu,trangThai) values 
+('Villa', 30, 99, 30, 900000, 1, 1,'booked'),
+('House', 20, 15, 45, 800000, 2, 2,'booked'),
+('House', 20, 15, 45, 800000, 3, 2,'booked'),
+('Room', 10, 66, 15, 500000, 4, 3, 'booked'),
+('Room', 5, 75, 15, 400000, 4, 3, 'booked'),
+('Villa', 50, 12, 6, 900000, 3, 1, 'booked'),
+('Villa', 60, 78, 69, 899000, 2, 1,'booked');
+insert into dichvudikem (tenDichVuDiKem, gia, donVi, trangThaiKhaDung) values
+('massage', 500000, 'VND', 'full'),
+('karaoke', 500000, 'VND', 'full'),
+('thuc an', 500000, 'VND', 'full'),
+('nuoc uong', 500000, 'VND', 'full'),
+('thue xe', 500000, 'VND', 'still');
+insert into hopdong (idNhanvien, idKhachHang, idDichVu,ngayLamHopDong, ngayKetThuc, tienDatCoc, tongTien) values
+(2, 2, 1,'2021-02-06', '2021-02-24', 5000000,1000000),
+(3, 1, 2,'2021-01-06', '2021-02-18', 6000000,1500000);
+insert into hopdongchitiet(idHopDong,idDichVuDiKem,soLuong) values
+(1,5,2),
+(2,1,5);
