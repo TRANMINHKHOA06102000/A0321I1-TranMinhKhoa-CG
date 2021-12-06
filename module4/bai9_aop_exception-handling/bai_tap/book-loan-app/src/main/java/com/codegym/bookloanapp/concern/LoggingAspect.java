e240b54cc6f8f878d96625180939fbb5c3f91cd8
TRANMINHKHOA06102000 <khoa4755@gmail.com>
2021-12-06 19:56:24 +0700

    up thuc hanh bai 10

--- module4/bai9_aop_exception-handling/bai_tap/book-loan-app/src/main/java/com/codegym/bookloanapp/concern/LoggingAspect.java
+++ module4/bai9_aop_exception-handling/bai_tap/book-loan-app/src/main/java/com/codegym/bookloanapp/concern/LoggingAspect.java
@@ -11,12 +11,14 @@ import java.util.logging.Logger;
 @Aspect
 @Component
 public class LoggingAspect {
+    //aspect ->advice -> pointcut -> joinpoint
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
     private static int action;
 
     //    Thực hiện ghi log tất cả các hành động khiến trạng thái sách của thư viện bị thay đổi.
     @AfterReturning("execution(* com.codegym.bookloanapp.service.BookService.save(..))")
     public void getBookLogAfterRunning(JoinPoint joinPoint) throws Throwable {
+        //String className= joinPoint.getTarget().getClass().getSimpleName();
         String methodName = joinPoint.getSignature().getName();
         logger.info("After returning: End call " + methodName);
         logger.info("[Log 1] -- Trạng thái sách thay đổi lúc " + LocalDateTime.now());
