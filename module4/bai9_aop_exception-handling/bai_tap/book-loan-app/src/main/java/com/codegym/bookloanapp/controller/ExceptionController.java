6fa0bbb280d5ff7c297f427114c53b92c242ebe5
TRANMINHKHOA06102000 <khoa4755@gmail.com>
2021-12-08 19:38:09 +0700

    up bai tap bai 10

--- module4/bai9_aop_exception-handling/bai_tap/book-loan-app/src/main/java/com/codegym/bookloanapp/controller/ExceptionController.java
+++ module4/bai9_aop_exception-handling/bai_tap/book-loan-app/src/main/java/com/codegym/bookloanapp/controller/ExceptionController.java
@@ -4,7 +4,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+@ControllerAdvice // xử lý exception trong hệ thống
 public class ExceptionController {
     @ExceptionHandler(Exception.class)
     public String showErrorPage(Exception e, Model model){
