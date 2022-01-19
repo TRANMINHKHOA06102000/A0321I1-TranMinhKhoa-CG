9279aa522f0048de07287b33df0169d96d5d056d
TRANMINHKHOA06102000 <khoa4755@gmail.com>
2022-01-19 19:23:40 +0700

    up bai thuc hanh 3

--- module5/3-angular-overview/thuc-hanh/practice-angular-overview/src/app/app.module.ts
+++ module5/3-angular-overview/thuc-hanh/practice-angular-overview/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {PetComponent} from "./pet/pet.component";
+import {FontSizeEditorComponent} from "./font-size-editor/profile.component";
+import {InstallBootstrapComponent} from "./install-bootstrap/install-bootstrap.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetComponent,
+    FontSizeEditorComponent,
+    InstallBootstrapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
