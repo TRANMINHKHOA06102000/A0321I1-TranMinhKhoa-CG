a2e0148afa9ae15ec67031e970a71d4a86f55465
TRANMINHKHOA06102000 <khoa4755@gmail.com>
2022-01-24 21:38:44 +0700

    up bài tâp bài 5

--- module5/5-components-interaction/exercises/src/app/app.component.spec.ts
+++ module5/5-components-interaction/exercises/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'exercise'`, () => {
+  it(`should have as title 'exercises'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('exercise');
+    expect(app.title).toEqual('exercises');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('exercise app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('exercises app is running!');
   });
 });
