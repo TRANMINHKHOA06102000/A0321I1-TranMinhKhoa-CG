2ebb449c89785987923957134b918b1f1ec81753
trankhoa2000 <khoa4755@gmail.com>
2022-02-11 16:21:43 +0700

    up bai thuc hanh 6'

--- module5/6-angular-form/practice/src/app/app.module.ts
+++ module5/6-angular-form/practice/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
