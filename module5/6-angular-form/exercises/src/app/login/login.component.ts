b3b29d71799b74173788fb8e21f94981d3f8ce97
trankhoa2000 <khoa4755@gmail.com>
2022-02-13 16:54:59 +0700

    up bai tap bai 6

---
+++ module5/6-angular-form/exercises/src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
+  }
+}
