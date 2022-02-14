33c6998ec6cd95b1ad728a01d8e37b6cd7019d63
trankhoa2000 <khoa4755@gmail.com>
2022-02-14 21:09:12 +0700

    up bai tap b7

--- module5/7-service-and-router/exercises/src/app/app.module.ts
+++ module5/7-service-and-router/exercises/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DictionaryPageComponent } from './dictionary-page/dictionary-page.component';
+import { DictionaryDetailComponent } from './dictionary-detail/dictionary-detail.component';
+import {DictionaryService} from "./services/dictionary.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DictionaryPageComponent,
+    DictionaryDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DictionaryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
