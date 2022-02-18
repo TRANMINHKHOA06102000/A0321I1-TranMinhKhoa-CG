ab6ebaa1035a6e8c4f9d34bc371e4cacdf900709
trankhoa2000 <khoa4755@gmail.com>
2022-02-18 21:16:59 +0700

    up case study

--- module5/0-case-study/furama-resort/src/app/app.module.ts
+++ module5/0-case-study/furama-resort/src/app/app.module.ts
@@ -5,12 +5,14 @@ import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
